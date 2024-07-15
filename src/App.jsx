0fb93fbc7cda8c650e0db0ded3a07772533662aa
Rukhsana <rukhsana21@navgurukul.org>
2024-07-15 18:03:59 +0530

    setup all the things and checkout my branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-600">
+    <h1 className="text-3xl font-bold underline bg-yellow-600">
       Hello world to you!
     </h1>
   )
